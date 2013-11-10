554608cc65e79fbd8cc31767e9eca9b5994ab135
kubicz10 <kubicz10@gmail.com>
2013-11-10 21:39:19 +0100

    Thymeleaf-springsecurity added to pom.xml. Added security awareness to views.

--- src/main/java/agh/sr/tweedle/model/SessionBean.java
+++ src/main/java/agh/sr/tweedle/model/SessionBean.java
@@ -11,18 +11,11 @@ import org.springframework.stereotype.Component;
 @Scope(proxyMode = ScopedProxyMode.TARGET_CLASS, value = "session")
 public class SessionBean implements Serializable {
 	private TwitterProfile twitterProfile;
-	private Object securityPrincipal;
 	
 	public TwitterProfile getTwitterProfile() {
 		return twitterProfile;
 	}
 	public void setTwitterProfile(TwitterProfile twitterProfile) {
 		this.twitterProfile = twitterProfile;
-	}
-	public Object getSecurityPrincipal() {
-		return securityPrincipal;
-	}
-	public void setSecurityPrincipal(Object securityPrincipal) {
-		this.securityPrincipal = securityPrincipal;
 	}
 }
