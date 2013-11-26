23d5c933d8648c4c908d95c53501424ac82b6886
wojtasskorcz <wojtasskorcz@gmail.com>
2013-11-26 18:30:40 +0100

    Restricted removing followed only to admin. First javadocs.

--- src/main/java/agh/sr/tweedle/controller/CustomConnectController.java
+++ src/main/java/agh/sr/tweedle/controller/CustomConnectController.java
@@ -1,30 +1,44 @@
 package agh.sr.tweedle.controller;
 
-
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.connect.web.ConnectController;
 
+/**
+ * Controller that intercepts Twitter connection changes and allows for
+ * implementation of custom logic when they arise. This implementation of the
+ * controller redirects user to "/" after connection status changes.
+ */
 public class CustomConnectController extends ConnectController {
 
-    public CustomConnectController(
-            ConnectionFactoryLocator connectionFactoryLocator,
-            ConnectionRepository connectionRepository) {
-        super(connectionFactoryLocator, connectionRepository);
-    }
-    
-    @Override
-    protected String connectedView(String providerId){
-    	return "redirect:/";
-    }
-    
-    @Override
-    protected String connectView(String providerId){
-    	return "redirect:/";
-    }
-    
-    @Override
-    protected String connectView(){
-    	return "redirect:/";
-    }
+	/**
+	 * Creates this controller.
+	 * 
+	 * @param connectionFactoryLocator
+	 *            the locator for ConnectionFactory instances needed to
+	 *            establish connections
+	 * @param connectionRepository
+	 *            the current user's ConnectionRepository needed to persist
+	 *            connections; must be a proxy to a request-scoped bean
+	 */
+	public CustomConnectController(
+			ConnectionFactoryLocator connectionFactoryLocator,
+			ConnectionRepository connectionRepository) {
+		super(connectionFactoryLocator, connectionRepository);
+	}
+
+	@Override
+	protected String connectedView(String providerId) {
+		return "redirect:/";
+	}
+
+	@Override
+	protected String connectView(String providerId) {
+		return "redirect:/";
+	}
+
+	@Override
+	protected String connectView() {
+		return "redirect:/";
+	}
 }
--- src/main/java/agh/sr/tweedle/controller/FollowedController.java
+++ src/main/java/agh/sr/tweedle/controller/FollowedController.java
@@ -26,83 +26,130 @@ import agh.sr.tweedle.model.SessionBean;
 import agh.sr.tweedle.util.TwitterConnectionUtils;
 import agh.sr.tweedle.validation.FollowUserFormValidator;
 
-
+/**
+ * Controls the flow of "/followed" URLs.
+ */
 @Controller
 @RequestMapping("/followed")
 public class FollowedController {
-	
-	private static final Logger logger = Logger.getLogger(FollowedController.class.getName());
-	
+
+	private static final Logger logger = Logger
+			.getLogger(FollowedController.class.getName());
+
 	@Autowired
-    private Twitter twitter;
-	
+	private Twitter twitter;
+
 	@Autowired
 	private FollowUserFormValidator followUserFormValidator;
-	
-	@Autowired
-    private TwitterConnectionUtils twitterConnectionUtils;
-	
-	@Autowired
-    private SessionBean sessionBean;
-	
-    @ModelAttribute("followedForm")
-    public FollowUserForm newFollowedForm() {
-        return new FollowUserForm();
-    }
 
-    @RequestMapping(method=RequestMethod.GET)
-    public String getFollowed(Model model) {
-        model.addAttribute("sessionBean", sessionBean);
-        CursoredList<TwitterProfile> followed = null;
-        try{
-            if (!twitterConnectionUtils.isConnectedToTwitter()) {
-        		return "redirect:/connect/twitter";
-            }	
-            followed = twitter.friendOperations().getFriends();
-        }
-        catch (Exception e){
+	@Autowired
+	private TwitterConnectionUtils twitterConnectionUtils;
+
+	@Autowired
+	private SessionBean sessionBean;
+
+	/**
+	 * Adds the {@link agh.sr.tweedle.model.FollowUserForm} to the model when
+	 * this controller processes requests.
+	 * 
+	 * @return {@link agh.sr.tweedle.model.FollowUserForm} object that should be
+	 *         added to the model
+	 */
+	@ModelAttribute("followedForm")
+	public FollowUserForm newFollowedForm() {
+		return new FollowUserForm();
+	}
+
+	/**
+	 * Requests rendering of the view that shows users followed on Twitter by
+	 * the user. If user is not connected to Twitter, returns Twitter logging
+	 * view.
+	 * 
+	 * @param model
+	 *            current model (injected by Spring)
+	 * @return name of the view to be rendered
+	 */
+	@RequestMapping(method = RequestMethod.GET)
+	public String getFollowed(Model model) {
+		model.addAttribute("sessionBean", sessionBean);
+		CursoredList<TwitterProfile> followed = null;
+		try {
+			if (!twitterConnectionUtils.isConnectedToTwitter()) {
+				return "redirect:/connect/twitter";
+			}
+			followed = twitter.friendOperations().getFriends();
+		} catch (Exception e) {
 			model.addAttribute("exception", e.toString());
 			return "index";
-        }
-    
-        model.addAttribute("followed", followed);      
-        return "twitter/followed";
-    }
-    
+		}
+
+		model.addAttribute("followed", followed);
+		return "twitter/followed";
+	}
+
+	/**
+	 * Sets the validator for this controller to
+	 * {@link agh.sr.tweedle.validation.FollowUserFormValidator}.
+	 * 
+	 * @param binder
+	 *            a WebDataBinder that binds web request parameters to JavaBean
+	 *            objects
+	 */
 	@InitBinder("followedForm")
-	private void initBinder(WebDataBinder binder){
+	private void initBinder(WebDataBinder binder) {
 		binder.setValidator(followUserFormValidator);
 	}
-    
-    @RequestMapping(method = RequestMethod.POST)
-    public String addFollowed(@Valid @ModelAttribute("followedForm") FollowUserForm followedForm, BindingResult result, final RedirectAttributes redirectAttributes){
-    	if (result.hasErrors()){
-            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.followedForm", result);
-    	    redirectAttributes.addFlashAttribute("followedForm", followedForm);
-    		return "redirect:/followed";
-    	}
-    	
-    	try{
-        	twitter.friendOperations().follow(followedForm.getScreenName());   		
-    	}
-        catch (Exception e){
-    	    redirectAttributes.addFlashAttribute("exception",  e.toString());
+
+	/**
+	 * Processes adding a new followed user. After the operation finishes
+	 * (successfully or not) returns the same view.
+	 * 
+	 * @param followedForm
+	 *            form containing the name of the user that is to be added
+	 * @param result
+	 *            result of the validation of the form
+	 * @param redirectAttributes
+	 *            attributes used to carry model information after redirecting
+	 * @return string that instructs Spring to redirect to "/followed"
+	 */
+	@RequestMapping(method = RequestMethod.POST)
+	public String addFollowed(
+			@Valid @ModelAttribute("followedForm") FollowUserForm followedForm,
+			BindingResult result, final RedirectAttributes redirectAttributes) {
+		if (result.hasErrors()) {
+			redirectAttributes
+					.addFlashAttribute(
+							"org.springframework.validation.BindingResult.followedForm",
+							result);
+			redirectAttributes.addFlashAttribute("followedForm", followedForm);
 			return "redirect:/followed";
-        }
-    	return "redirect:/followed";
-    }
-    
-    @RequestMapping(value= "/{id}", method = RequestMethod.DELETE, produces="application/json; charset=utf-8")
-    @ResponseBody
-    public String removeFollowed(@PathVariable("id") String userId){
-    	Long id = Long.parseLong(userId);
-    	try{
-        	twitter.friendOperations().unfollow(id);  		
-    	}
-        catch (Exception e){
-        	return String.format("{\"exception\": \"Remove user %ld failed\"}", id);
-        }
-		
-    	return "{\"ok\": \"true\"}";   	
-    }
+		}
+
+		try {
+			twitter.friendOperations().follow(followedForm.getScreenName());
+		} catch (Exception e) {
+			redirectAttributes.addFlashAttribute("exception", e.toString());
+			return "redirect:/followed";
+		}
+		return "redirect:/followed";
+	}
+
+	/**
+	 * Processes a request to remove a followed user.
+	 * @param userId ID of the user that is to be removed
+	 * @return JSON with the status of the operation
+	 */
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "application/json; charset=utf-8")
+	@ResponseBody
+	public String removeFollowed(@PathVariable("id") String userId) {
+		Long id = Long.parseLong(userId);
+		try {
+			twitter.friendOperations().unfollow(id);
+		} catch (Exception e) {
+			return String.format("{\"exception\": \"Remove user %ld failed\"}",
+					id);
+		}
+
+		return "{\"ok\": \"true\"}";
+	}
 }
